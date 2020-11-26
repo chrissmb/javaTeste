c41ea2542e697980fa326cd0980f3c6b980834e4
chrissantista@gmail.com <chrissantista@gmail.com>
2020-11-26 20:22:01 +0000

    map e java 1.8

--- src/main/java/org/chrissmb/Matrix.java
+++ src/main/java/org/chrissmb/Matrix.java
@@ -1,5 +1,7 @@
 package org.chrissmb;
 
+import java.util.function.Function;
+
 public class Matrix {
 
     private Double[][] data;
@@ -13,5 +15,21 @@ public class Matrix {
             return 0d;
         }
         return data[row][col];
+    }
+
+    public void set(int row, int col, double v) {
+        data[row][col] = v;
+    }
+
+    public void map(Function<Double, Double> fn) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] = isNull(fn.apply(isNull(data[i][j])));
+            }
+        }
+    }
+
+    private double isNull(Double v) {
+        return v == null ? 0d : v;
     }
 }
