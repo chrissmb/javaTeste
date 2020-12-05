89f7048999e09c9ad5a2a4b56a1e49b5c99484e1
chrissantista@gmail.com <chrissantista@gmail.com>
2020-12-05 15:44:14 +0000

    teste multiplicar com falha

--- src/test/java/org/chrissmb/AppTest.java
+++ src/test/java/org/chrissmb/AppTest.java
@@ -20,8 +20,17 @@ public class AppTest {
     @Test
     public void testSum() {
         Matrix m1 = new Matrix(new Double[][] {{2d, 3d}, {4d, 5d}});
-        Matrix m2 = new Matrix(new Double[][] {{1d,2d}, {3d, 4d}});
+        Matrix m2 = new Matrix(new Double[][] {{1d, 2d}, {3d, 4d}});
         Matrix expected = new Matrix(new Double[][] {{3d, 5d}, {7d, 9d}});
+        m1.sum(m2);
+        assertEquals(expected, m1);
+    }
+
+    @Test
+    public void testMultiply() {
+        Matrix m1 = new Matrix(new Double[][] {{2d, 3d}, {4d, 5d}});
+        Matrix m2 = new Matrix(new Double[][] {{1d, 2d}, {3d, 4d}});
+        Matrix expected = new Matrix(new Double[][] {{17d, 22d}, {10d, 13d}});
         m1.sum(m2);
         assertEquals(expected, m1);
     }
